56c754abd4c771499403e6377170dc5ed61f419d
navib <hillpote@gamil.com>
2026-07-11 11:37:26 +0800

    refactor(road-system): 移除未使用的 Set 导入
    - 删除了未被引用的 java.util.Set 导入语句
    - 清理了代码中的无用依赖项
    - 提高了代码的整洁性

--- src/main/java/com/plot/plugin/RoadSystemPlugin.java
+++ src/main/java/com/plot/plugin/RoadSystemPlugin.java
@@ -59,7 +59,6 @@ import java.util.Comparator;
 import java.util.HexFormat;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Consumer;
 
 public class RoadSystemPlugin extends Plugin {
